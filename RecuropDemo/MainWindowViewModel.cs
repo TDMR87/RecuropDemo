aea12dabc9b068264e35df6d70d3ba8f4c2aa23f
Diego Rönkkömäki <diego.ronkkomaki@laavusolutions.fi>
2021-03-02 23:15:16 +0200

    Add project files.

---
+++ RecuropDemo/MainWindowViewModel.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.Windows;
+using Recurop;
+
+namespace RecuropDemo
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        // Public properties
+        public RecurringOperation RecurringTimerOperation { get; }
+        public RecurringOperation RecurringVisibilityOperation { get; }
+        public ObservableCollection<TimeSpan> LapTimes { get; set; }
+        public ObservableCollection<string> ExceptionMessages { get; set; }
+        public TimeSpan DisplayTimer
+        {
+            get => _displayTimer ?? TimeSpan.Zero;
+            private set
+            {
+                _displayTimer = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public Visibility TimerVisibility
+        {
+            get => timerVisibility;
+            set
+            {
+                timerVisibility = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // Private fields
+        private int elapsedSeconds;
+        private bool throwException;
+        private TimeSpan? _displayTimer;
+        private Visibility timerVisibility;
+
+        // Viewmodel construtor
+        public MainWindowViewModel()
+        {
+            // Initialize collections
+            LapTimes = new ObservableCollection<TimeSpan>();
+            ExceptionMessages = new ObservableCollection<string>();
+
+            // Initialize the timer operation
+            RecurringTimerOperation = new RecurringOperation(name: "TimerOperation");
+            RecurringTimerOperation.OperationFaulted += OnOperationFaulted;
+            RecurringTimerOperation.StatusChanged += OnOperationStatusChanged;
+
+            // Initialize the blink operation
+            RecurringVisibilityOperation = new RecurringOperation(name: "BlinkOperation");
+        }
+
+        /// <summary>
+        /// Method for handling the OperationFaulted event.
+        /// </summary>
+        /// <param name="ex"></param>
+        private void OnOperationFaulted(Exception ex)
+        {
+            // Because this exception event handler delegate is invoked on a
+            // threadpool thread, we can only modify the list on the
+            // thread it was created on (in this case the UI thread).
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                ExceptionMessages.Add(ex.Message);
+            });
+        }
+
+        /// <summary>
+        /// Method for handling the StatusChanged event.
+        /// </summary>
+        private void OnOperationStatusChanged()
+        {
+            if (RecurringTimerOperation.Status == RecurringOperationStatus.Cancelled)
+            {
+
+            }
+        }
+
+        public ICommand StartTimerCommand => new RelayCommand(() =>
+        {
+            // If operation is already started, do nothing
+            //if (!RecurringTimerOperation.CanBeStarted) return;
+
+            // Start a recurring operation using a lambda style Action.
+            // Alternatevly, you could call any named method instead.
+            RecurringOperations.Manager.StartRecurring(RecurringTimerOperation, TimeSpan.FromSeconds(1), () =>
+            {
+                // Increment elapsed seconds
+                elapsedSeconds++;
+
+                // Set value to the DisplayTimer
+                DisplayTimer = TimeSpan.FromSeconds(elapsedSeconds);
+
+                // Simulate a little work
+                Thread.Sleep(500);
+
+                // If an exception should be thrown
+                if (throwException)
+                {
+                    // Disable it
+                    throwException = false;
+
+                    // Throw an exception inside the recurring operation
+                    throw new InvalidOperationException(
+                        "An exception was thrown inside the recurring operation.");
+                }
+            });
+        });
+
+        public ICommand PauseTimerCommand => new RelayCommand(() =>
+        {
+            // Pause the timer operation
+            RecurringOperations.Manager.PauseRecurring(RecurringTimerOperation);
+
+            // Start a blink operation
+            RecurringOperations.Manager.StartRecurring(RecurringVisibilityOperation, TimeSpan.FromSeconds(0.5), () =>
+            {
+                switch (TimerVisibility)
+                {
+                    case Visibility.Visible:
+                        TimerVisibility = Visibility.Hidden;
+                        break;
+                    case Visibility.Hidden:
+                        TimerVisibility = Visibility.Visible;
+                        break;
+                    case Visibility.Collapsed:
+                        break;
+                    default:
+                        break;
+                }
+            });
+        });
+
+        public ICommand ContinueTimerCommand => new RelayCommand(() =>
+        {
+            // Resume the timer operation
+            RecurringOperations.Manager.ResumeRecurring(RecurringTimerOperation);
+
+            // Cancel the blink operation
+            RecurringOperations.Manager.Cancel(RecurringVisibilityOperation);
+        });
+
+        public ICommand CancelTimerCommand => new RelayCommand(() =>
+        {
+            RecurringOperations.Manager.Cancel(RecurringTimerOperation);
+
+            DisplayTimer = TimeSpan.Zero;
+
+            elapsedSeconds = 0;
+        });
+
+        public ICommand ThrowCommand => new RelayCommand(() =>
+        {
+            throwException = true;
+        });
+
+        public ICommand LapTimeCommand => new RelayCommand(() =>
+        {
+            LapTimes.Add(DisplayTimer);
+        });
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
